013ee951004db6ab0260ba3304b7bf6d517be9c1
Robin <rrlangton@gmail.com>
2024-04-21 20:50:30 -0500

    updates to sequelize

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,8 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_USERNAME,
+    process.env.DB_NAME,
+    process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
       host: 'localhost',
